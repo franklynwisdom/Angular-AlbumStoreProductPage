26e2c7fa16e375505ee8aeb409b1718e86b40b85
Franklyn <franklynwisdom@gmail.com>
2020-09-29 07:26:01 +0100

    create a private variable _albumUrl

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, ResponseOptions } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private  _albumUrl: '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   getAlbum(id:number){
